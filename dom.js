65e530d576bb4f9ecfb6d2e9731081f304455e26
Sudip Mishra <sudipmishra1000@gmail.com>
2023-03-15 08:00:54 +0530

    Add items Page

---
+++ dom.js
@@ -0,0 +1,6 @@
+// Change the title of the page
+document.title = "New Title";
+
+// Add a black border to the title
+const headerTitle = document.querySelector("#header-title");
+headerTitle.style.border = "1px solid black";
